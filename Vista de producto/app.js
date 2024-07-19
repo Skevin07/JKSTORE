77d3464f89da07837c478ed6f830d79001a1307d
Kevin Rodriguez <kevinportillo808@gmail.com>
2024-07-19 14:33:09 -0600

    ADD archivo js
    Se añadio el archivo javascrip para la vista de prodcutos

---
+++ Vista de producto/app.js
@@ -0,0 +1,11 @@
+let darkmode = document.querySelector('#darkmode');
+
+darkmode.onclick = any = {
+    if(darkmode.classList.contains('ri-moon-fill')){
+    darkmode.classList.replace('ri-moon-fill', 'rin-son-fill');
+    document.body.classList.add('color')
+}else {
+    darkmode.classList.replace('rin-sun-fill', 'rin-moon-fill');
+    document.body.classList.remove('coloe')
+}
+}
