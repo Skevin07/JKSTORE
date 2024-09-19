e5bf1ad5322f108f7c320a87c09a9a0bc7335538
MilitoJR <bl0049032019@unab.edu.sv>
2024-09-18 19:11:37 -0600

    1.1

--- TiendaJK/TiendaJK/obj/Debug/net8.0/TiendaJK.AssemblyInfo.cs
+++ TiendaJK/TiendaJK/obj/Debug/net8.0/TiendaJK.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TiendaJK")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7942ff8bcdb5476ae35f4266f283ebdc9df39186")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecd3c7d15dc6134802a72c457e3713f338a0af52")]
 [assembly: System.Reflection.AssemblyProductAttribute("TiendaJK")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TiendaJK")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
